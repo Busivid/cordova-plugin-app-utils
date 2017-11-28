bda7853b7821e54132595a06e69132640ed232f3
root <root@ryan.dev.busivid.com>
2017-11-28 15:44:28 +1100

    Whitespace

--- src/android/AppUtils.java
+++ src/android/AppUtils.java
@@ -1,44 +1,38 @@
 package com.busivid.cordova.apputils;
 
 import android.app.Activity;
-import android.content.pm.ApplicationInfo;
 import android.content.Intent;
-import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Build;
-import android.provider.Telephony;
 
-import java.lang.reflect.Method;
-import java.lang.String;
-import java.util.ArrayList;
-
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 public class AppUtils extends CordovaPlugin {
-	
 	private static final int EMAIL_INTENT_RESULT = 1000;
 	private static final int SMS_INTENT_RESULT = 1001;
 
 	private CallbackContext callbackContext; // The callback context from which we were invoked.
-	
+
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-	
 		this.callbackContext = callbackContext;
-		
+
 		if (action.equals("BundleInfo")) {
 			try {
- 				JSONObject bundleInfo = this.bundleInfo();
+				JSONObject bundleInfo = this.bundleInfo();
 				callbackContext.success(bundleInfo);
 			} catch (NameNotFoundException exception) {
-				callbackContext.error(exception.getMessage());	
+				callbackContext.error(exception.getMessage());
 			}
 		} else if(action.equals("ComposeEmail")) {
 			JSONObject jsonObject = args.getJSONObject(0);
@@ -47,8 +41,8 @@ public class AppUtils extends CordovaPlugin {
 			composeEmail(jsonObject.getString("body"), jsonObject.getString("subject"), recipients);
 		} else if(action.equals("ComposeSMS")) {
 			JSONObject jsonObject = args.getJSONObject(0);
-                        JSONArray recipientsJson = jsonObject.getJSONArray("recipients");
-                        String[] recipients = convertJsonArrayToStringArray(recipientsJson);
+			JSONArray recipientsJson = jsonObject.getJSONArray("recipients");
+			String[] recipients = convertJsonArrayToStringArray(recipientsJson);
 			composeSMS(jsonObject.getString("body"), recipients);
 		} else if (action.equals("DeviceInfo")) {
 			JSONObject deviceInfo = new JSONObject();
@@ -65,7 +59,7 @@ public class AppUtils extends CordovaPlugin {
 
 		Activity cordovaActivity = this.cordova.getActivity();
 		String packageName = cordovaActivity.getPackageName();
-		
+
 		PackageInfo packageInfo = cordovaActivity.getPackageManager().getPackageInfo(packageName, 0);
 
 		boolean isDebug = (cordovaActivity.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
@@ -73,7 +67,7 @@ public class AppUtils extends CordovaPlugin {
 		results.put("bundleVersion", packageInfo.versionCode);
 		results.put("bundleId", packageName);
 		results.put("bundleIsDebug", isDebug);
-		return results;	
+		return results;
 	}
 
 	public static String getHostname() {
@@ -92,7 +86,7 @@ public class AppUtils extends CordovaPlugin {
 			for(int i = 0; i < jsonArray.length(); i++) {
 				list.add((String)jsonArray.get(i));
 			}
-		
+
 			return list.toArray(new String[list.size()]);
 		} catch (JSONException exception) {
 			return new String[0];
@@ -117,18 +111,18 @@ public class AppUtils extends CordovaPlugin {
 		final CordovaPlugin plugin = (CordovaPlugin) this;
 		Runnable worker = new Runnable() {
 			public void run() {
-                		Intent emailIntent = new Intent(Intent.ACTION_SEND);
+				Intent emailIntent = new Intent(Intent.ACTION_SEND);
 				emailIntent.setData(Uri.parse("mailto:"));
 				emailIntent.setType("message/rfc822");
 				//emailIntent.setType("text/html");
 				emailIntent.putExtra(Intent.EXTRA_EMAIL, recipients);
 				emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
 				emailIntent.putExtra(Intent.EXTRA_TEXT, android.text.Html.fromHtml(body));
-                		emailIntent.putExtra(Intent.EXTRA_HTML_TEXT, android.text.Html.fromHtml(body));
+				emailIntent.putExtra(Intent.EXTRA_HTML_TEXT, android.text.Html.fromHtml(body));
 				plugin.cordova.startActivityForResult(plugin, emailIntent, EMAIL_INTENT_RESULT);
-            		}
-        	};
-        	this.cordova.getThreadPool().execute(worker);
+			}
+		};
+		this.cordova.getThreadPool().execute(worker);
 	}
 
 	public void composeSMS(final String body, final String[] recipients) {
